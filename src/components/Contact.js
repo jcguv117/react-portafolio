135947d3f4f7c92586804a70843f630d86149965
Carlos Guevara <juancguv@gmail.com>
2022-12-24 01:30:22 -0600

    form netlify hidden

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ export default function Contact() {
         <h2 className="text-zinc-400">CONTACTAME</h2>
         <div className="flex items-center justify-center m-2 p-2 text-white">
             <form name="contact" method="POST" className="grid gap-x-8 gap-y-2 w-full" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                 <div className="w-full">
                     <input className="bg-cyan-900 rounded-xl m-1 p-2 w-full md:w-2/3 lg:w-1/2" type="text" name="name" placeholder="Nombre" />
                 </div>
